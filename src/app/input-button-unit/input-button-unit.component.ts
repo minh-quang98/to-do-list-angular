19ab028a595aa3ef02e04ab24def130515a7fab6
minh <minhntn@pmr.vn>
2023-08-14 15:13:41 +0700

    event binding

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
   templateUrl: './input-button-unit.component.html',
   styleUrls: ['./input-button-unit.component.scss']
 })
-export class InputButtonUnitComponent {
+export class InputButtonUnitComponent implements OnInit{
   title = "Hello world"
+
+  constructor() {
+    this.changeTitle("Show me the money")
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.changeTitle('This is not the title you are looking for');
+    }, 3000);
+  }
+
+  changeTitle(newTitle: string) {
+    this.title = newTitle
+  }
+
+  getInputValue(event: Event) {
+    return (event.target as HTMLInputElement).value;
+  }
 }
